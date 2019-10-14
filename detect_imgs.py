15bbc5719c8a519728311eaca25c2f061e6417a6
qxc <qxc@meitu.com>
2019-10-14 18:52:40 +0800

    Add time statistics

--- detect_imgs.py
+++ detect_imgs.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 import cv2
-
+import time
 from vision.ssd.config.fd_config import define_img_size
 
 parser = argparse.ArgumentParser(
@@ -22,7 +22,7 @@ parser.add_argument('--candidate_size', default=1500, type=int,
                     help='nms candidate size')
 parser.add_argument('--path', default="imgs", type=str,
                     help='imgs dir')
-parser.add_argument('--test_device', default="cuda:0", type=str,
+parser.add_argument('--test_device', default="cpu", type=str,
                     help='cuda:0 or cpu')
 args = parser.parse_args()
 define_img_size(args.input_size)  # must put define_img_size() before 'import create_mb_tiny_fd, create_mb_tiny_fd_predictor'
@@ -54,11 +54,19 @@ if not os.path.exists(result_path):
     os.makedirs(result_path)
 listdir = os.listdir(args.path)
 sum = 0
+avg_time = 0
+img_num = 0
 for file_path in listdir:
     img_path = os.path.join(args.path, file_path)
     orig_image = cv2.imread(img_path)
     image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+    start = time.time()
     boxes, labels, probs = predictor.predict(image, args.candidate_size / 2, args.threshold)
+    end = time.time()
+    print('time used:', end - start)
+    img_num = img_num + 1
+    avg_time = avg_time + end - start
+
     sum += boxes.size(0)
     for i in range(boxes.size(0)):
         box = boxes[i, :]
@@ -69,4 +77,7 @@ for file_path in listdir:
     cv2.putText(orig_image, str(boxes.size(0)), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     cv2.imwrite(os.path.join(result_path, file_path), orig_image)
     print(f"Found {len(probs)} faces. The output image is {result_path}")
+    print('avg_time:', avg_time / img_num)
+
+
 print(sum)
